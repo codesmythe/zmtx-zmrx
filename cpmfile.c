0a9761cddab0c79dec9ba68663d162b2fd6e6b52
lynchaj <86925539+lynchaj@users.noreply.github.com>
2021-10-31 09:49:52 -0400

    Update cpmfile.c
    attempt adding get_file_size() function for CP/M 2.2

--- cpmfile.c
+++ cpmfile.c
@@ -2,6 +2,7 @@
 
 #include "fileio.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 long fileio_get_modification_time(const char *filename) { return -1L; }
 
@@ -12,3 +13,30 @@ long get_file_size(FILE *fp) {
     exit(1);
     return -1;
 }
+
+/* remove comments to implement get_file_size() function
+
+// code sample from geeksforgeeks modified for zmtx
+
+long get_file_size(FILE *fp)
+{
+    // assumes file fp is already open
+    
+    // checking if the file exist or not
+    if (fp == NULL) {
+        printf("File Not Found!\n");
+        return -1;
+    }
+  
+    fseek(fp, 0L, SEEK_END);
+  
+    // calculating the size of the file
+    long res = ftell(fp);
+  
+    // closing the file
+    fclose(fp);
+  
+    return res;
+}
+
+*/
