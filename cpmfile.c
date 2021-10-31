defecff635b53f49fa09b4bd85eee288e363aa31
lynchaj <86925539+lynchaj@users.noreply.github.com>
2021-10-31 15:02:11 -0400

    Update cpmfile.c
    Provide alternative approach to computing file size in CP/M in case more traditional methods of computing file size do not work

--- cpmfile.c
+++ cpmfile.c
@@ -2,6 +2,7 @@
 
 #include "fileio.h"
 #include <stdlib.h>
+#include <cpm.h>
 
 long fileio_get_modification_time(const char *filename) { return -1L; }
 
@@ -12,3 +13,22 @@ long get_file_size(FILE *fp) {
     exit(1);
     return -1;
 }
+
+
+/*
+
+long get_file_size(FILE *fp) {
+
+    long size;
+
+    int F_SIZE = 35;  // BDOS function call for file size in sectors
+    
+    int sector_size = 128;  // sectors in CP/M are 128 bytes each
+    
+    // compute maximum file size (number of sectors in file) * sector size 
+    size = bdos(F_SIZE, *getfcb(void)) * sector_size;
+    
+    return size;
+}
+
+*/
