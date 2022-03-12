59c70bf180d01c22871a355a5672a9f39c90a338
Rob Gowin <robgowin@gmail.com>
2022-03-12 16:46:23 -0600

    TOS port: Use our own large receive buffer.

--- tosterm.c
+++ tosterm.c
@@ -4,6 +4,8 @@
 
 #include <setjmp.h>
 #include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 #include "zmodem.h"
@@ -11,24 +13,100 @@
 
 #include <osbind.h>
 
+#define IBUFSIZ               32000 /* Size of Rs232 receive buffer, can't exceed 32767. */
+
+extern int opt_d;
 extern int last_sent;
 
-const int AUX_DEV = 11;
+const int AUX_DEV = 10;
 const int CONSOLE_DEV = 2;
+
+int fd_init_done = 0;
+
+#ifdef DEBUG
+extern int raw_trace;
+
+#endif
+
+_IOREC save,     /* the original Iorec is saved here for the duration of this process */
+     *savep;     /* ptr returned by Iorec() */
+
+char iobuf[IBUFSIZ]; /* RS232 receive buffer. */
 
 /*
  * routines to make the io channel raw and restore it
  * to its normal state.
  *
- * No-ops on TOS.
+ * No-ops on the Atari ST.
  */
 
-void fd_init(void)
+int16_t has_bconmap(void)
 {
+    return (0L == Bconmap(0));
+}
+
+void fd_init(void) {
+
+    if (!has_bconmap()) {
+        fprintf(stderr, "BCONMAP not supported. That is unexpected.\r\n");
+        return;
+    }
+
+    /* Map in our device to that Iorec and Rsconf act on it. */
+    int32_t old_bconmap_dev = Bconmap(AUX_DEV);
+
+    /* Get pointer to Rs232 input record */
+    savep = (_IOREC * )Iorec(0);
+
+    /* Save the info */
+    save.ibuf = savep->ibuf;
+    save.ibufsiz = savep->ibufsiz;
+    save.ibufhd = savep->ibufhd;
+    save.ibuftl = savep->ibuftl;
+    save.ibuflow = savep->ibuflow;
+    save.ibufhi = savep->ibufhi;
+
+    /* Install my buffer in its place */
+    savep->ibuf = &iobuf[0];
+    savep->ibufsiz = IBUFSIZ;
+    savep->ibuflow = IBUFSIZ / 8;
+    savep->ibufhi = (IBUFSIZ / 4) * 3;
+    savep->ibufhd = savep->ibuftl = 0;
+
+    /* Turn on hardware flow control. */
+    const int flow_ctrl_hard = 2;
+    Rsconf(-1, flow_ctrl_hard, -1, -1, -1, -1);
+
+    Bconmap(old_bconmap_dev);
+
+    fd_init_done = 1;
 }
 
 void fd_exit(void)
 {
+    if (!has_bconmap()) {
+        fprintf(stderr, "BCONMAP not supported. That is unexpected.\r\n");
+        return;
+    }
+
+    if (!fd_init_done) return;
+
+    /* Reset to saved system buffer. */
+    /* FIXME: This isn't safe in the presence of RS232 interrupts. Make it code that is executed as supervisor? */
+    savep->ibuf     = save.ibuf;
+    savep->ibufsiz  = save.ibufsiz;
+    savep->ibuflow  = save.ibuflow;
+    savep->ibufhi   = save.ibufhi;
+    savep->ibufhd   = save.ibufhd;
+    savep->ibuftl   = save.ibuftl;
+
+    /* Map in our device so that Rsconf acts on it. */
+    int32_t old_bconmap_dev = Bconmap(AUX_DEV);
+    /* Turn off hardware flow control. There's no way to restore previous value. */
+    Rsconf(-1, 0, -1, -1, -1, -1);
+
+    Bconmap(old_bconmap_dev);
+
 }
 
 /*
@@ -144,7 +222,7 @@ int read_modem(unsigned char *buf, int count) {
     }
 }
 
-unsigned char inputbuffer[1024];
+unsigned char inputbuffer[IBUFSIZ];
 size_t n_in_inputbuffer = 0;
 int inputbuffer_index;
 
@@ -182,7 +260,7 @@ int rx_raw(int timeout)
         }
 
         stalarm(timeout_secs);
-        n_in_inputbuffer = read_modem(inputbuffer,1024);
+        n_in_inputbuffer = read_modem(inputbuffer,IBUFSIZ);
         stalarm(0);
 
         if (n_in_inputbuffer <= 0) {
