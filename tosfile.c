7f9bac4c9120e0c38c2eb4622d02bd1da5583a53
Rob Gowin <robgowin@gmail.com>
2022-03-19 12:09:05 -0500

    Implement file stat related stuff for TOS.
    Also, use ext.h instead of sys/stat.h. This fixes another
    part of #10.

--- tosfile.c
+++ tosfile.c
@@ -2,28 +2,29 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
-#include <sys/stat.h>
+#include <ext.h>
 
 #include "fileio.h"
 
 int use_aux = 0;
 
 long fileio_get_modification_time(const char *filename) {
-    return 0;
-#if 0
     FILE *fp = fopen(filename, "rb");
     if (fp == NULL) return -1;
     struct stat s;
     fstat(fileno(fp), &s);
+    fclose(fp);
     return s.st_mtime;
-#endif
 }
 
 void fileio_set_modification_time(const char *filename, long mdate) {
 }
 
 long get_file_size(FILE *fp) {
-     return 0;
+    if (fp == NULL) return 0;
+    struct stat s;
+    fstat(fileno(fp), &s);
+    return (long) s.st_size;
 }
 
 char *strip_path(char *path_in)
