42dbf0661a3baed71d13a856f3a49e70dc997b59
Rob Gowin <robgowin@gmail.com>
2022-05-27 13:53:14 -0500

    Use parens around macro aruguments.

--- crctab.h
+++ crctab.h
@@ -14,6 +14,6 @@
 extern unsigned short int crc16tab[0x100];
 extern unsigned long crc32tab[0x100];
 
-#define UPDCRC16(cp, crc) (crc16tab[((crc >> 8) & 255)] ^ (crc << 8) ^ cp)
+#define UPDCRC16(cp, crc) (crc16tab[(((crc) >> 8) & 255)] ^ ((crc) << 8) ^ (cp))
 
-#define UPDCRC32(b, c) (crc32tab[((int)c ^ b) & 0xff] ^ ((c >> 8) & 0x00FFFFFF))
+#define UPDCRC32(b, c) (crc32tab[((int)(c) ^ (b)) & 0xff] ^ (((c) >> 8) & 0x00FFFFFF))
